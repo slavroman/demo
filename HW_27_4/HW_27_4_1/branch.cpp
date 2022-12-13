ee353b28cb28e8cfddfcd7f680dcd7d43ab6a376
slavroman <slaventiy2000@gmail.com>
2022-12-13 11:58:29 +0900

    delete destructor

---
+++ HW_27_4/HW_27_4_1/branch.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+#include "branch.h"
+
+Branch::Branch()
+    : elfName("Unknown"), parent(nullptr), childrensCount(0), neighbours(0)
+{
+}
+
+void Branch::addChild(Branch* child)
+{
+    childrens.push_back(child);
+}
+
+void Branch::setElfName()
+{
+    std::string name = "";
+    std::cout << "Please enter elf's name:\n";
+    std::cin >> name;
+
+    assert(!name.empty());
+    elfName = name;
+
+    if (elfName != "None" || elfName != "none")
+    {
+        neighbours++;
+    }
+}
+
+void Branch::setParent(Branch* ptrParent)
+{
+    parent = ptrParent;
+}
+
+void Branch::setNeighbours(size_t number)
+{
+    neighbours = number;
+}
+
+void Branch::setChildrensCount(size_t number)
+{
+    childrensCount = number;
+}
+
+Branch* Branch::getChild(size_t number)
+{
+    if (number < 0 || number >= childrens.size())
+    {
+        std::cerr << "The number is out of range!!!\n";
+    }
+
+    return childrens[number];
+}
+
+std::string Branch::getElfName()
+{
+    return elfName;
+}
+
+Branch* Branch::getParent()
+{
+    return parent;
+}
+
+size_t Branch::getNeighbours()
+{
+    return neighbours;
+}
+
+size_t Branch::getChildrensCount()
+{
+    return childrensCount;
+}
+
+Branch* Branch::getTopBranch()
+{
+    if (parent == nullptr)
+    {
+        return nullptr;
+    }
+
+    if (parent->parent == nullptr)
+    {
+        return parent;
+    }
+    // if a small branch
+    return parent->getTopBranch();    
+}
---
+++ HW_27_4/HW_27_4_1/branch.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Branch
+{
+public:
+    Branch();    
+    
+    void addChild(Branch*);
+    void setElfName();
+    void setParent(Branch*);
+    void setNeighbours(size_t);
+    void setChildrensCount(size_t);
+
+    Branch*     getChild(size_t);
+    std::string getElfName();
+    Branch*     getParent();    
+    size_t      getNeighbours();
+    size_t      getChildrensCount();
+    Branch*     getTopBranch();
+
+private:
+    std::string          elfName;
+    Branch*              parent;
+    size_t               childrensCount;
+    size_t               neighbours;
+    std::vector<Branch*> childrens;
+};
