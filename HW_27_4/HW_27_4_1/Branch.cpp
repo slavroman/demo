4d970a44b5261c66abe41c6aee045409ee1d9907
slavroman <slaventiy2000@gmail.com>
2022-12-27 17:19:02 +0900

    1st redaction done

--- HW_27_4/HW_27_4_1/Branch.cpp
+++ HW_27_4/HW_27_4_1/Branch.cpp
@@ -2,24 +2,71 @@
 #include <cassert>
 #include "branch.h"
 
-
-
 Branch::Branch()
-    : elfName("none"), parent(nullptr)
+    : m_name("None"), m_parent(nullptr), m_nonEmptyHousesOnBranch(0)
 {
+}
+
+void Branch::setName()
+{
+    std::string elfName{ "" };
+
+    std::cout << "Please input elf name:\n";
+    std::cin >> elfName;
+
+    while (elfName.empty())
+    {
+        std::cout << "The elf's name couldn't be empty! Try again:\n";
+        std::cin >> elfName;
+    }
+
+    if (elfName != "None" || elfName != "none")
+    {
+        m_name = elfName;
+    }
+}
+
+void Branch::setParrent(Branch* parrent)
+{
+    m_parent = parrent;
+}
+
+void Branch::addChild(Branch* child)
+{
+    m_childrens.push_back(child);
+}
+
+std::string Branch::getName()
+{
+    return m_name;
+}
+
+size_t Branch::getCountOfNonEmptyHousesOnBranch()
+{
+    for (const auto& it : m_childrens)
+    {
+        if (it->getName() != "None")
+        {
+            m_nonEmptyHousesOnBranch++;
+        }
+    };
+
+    return m_nonEmptyHousesOnBranch;
 }
 
 Branch* Branch::getTopBranch()
 {
-    if (parent == nullptr)
+    // if tree
+    if (m_parent == nullptr)
     {
         return nullptr;
     }
 
-    if (parent->parent == nullptr)
+    // if a middle branch
+    if (m_parent->m_parent == nullptr)
     {
-        return parent;
+        return m_parent;
     }
     // if a small branch
-    return parent->getTopBranch();    
+    return m_parent->getTopBranch();    
 }
--- HW_27_4/HW_27_4_1/HW_27_4_Task_1.cpp
+++ HW_27_4/HW_27_4_1/HW_27_4_Task_1.cpp
@@ -2,20 +2,77 @@
 #include <cassert>
 #include <string>
 #include <vector>
+#include <memory>
 #include "branch.h"
 
-size_t randomInit(size_t, size_t);
+size_t randomInit(const size_t, const size_t);
 
 int main()
 {
 	std::srand((size_t)std::time(nullptr));
 
-	
+	Branch tree[5];
+
+	for (size_t itTree = 0; itTree < 5; itTree++)
+	{
+		for (size_t itBigBranch = 0; itBigBranch < randomInit(3, 5); itBigBranch++)
+		{
+			auto bigBranch = new Branch;
+			bigBranch->setName();
+			bigBranch->setParrent(&tree[itTree]);
+
+			for (size_t itMiddleBranch = 0; itMiddleBranch < randomInit(2, 3); itMiddleBranch++)
+			{
+				auto middleBranch = new Branch;
+				middleBranch->setName();
+				middleBranch->setParrent(bigBranch);
+				bigBranch->addChild(middleBranch);
+			}
+
+			tree[itTree].addChild(bigBranch);
+		}		
+	}
+
+	std::string elfNameToFind = "";
+	std::cout << "Please input elf name to find:\n";
+	std::cin >> elfNameToFind;
+	size_t neighbours = 0;
+	bool found = false;
+
+	for (size_t itTree = 0; itTree < 5 && !found; itTree++)
+	{
+		while (tree[itTree].getTopBranch())
+		{
+			neighbours += tree[itTree].getCountOfNonEmptyHousesOnBranch();
+
+			if (tree[itTree].getName() == elfNameToFind)
+			{
+				found = true;
+			}
+			
+		}
+
+		if (found)
+		{
+			break;
+		}
+		
+		neighbours = 0;
+	}
+
+	if (found)
+	{
+		std::cout << elfNameToFind << " has " << neighbours - 1 << " nieghbors.\n";
+	}
+	else
+	{
+		std::cout << "Elf not found.\n";
+	}
 
 	return 0;
 }
 
-size_t randomInit(size_t min, size_t max)
+size_t randomInit(const size_t min, const size_t max)
 {
     return (min + (rand() % static_cast<int>(max - min + 1)));
 }
