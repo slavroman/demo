052f1fe224919b9f05c5a74c4d314b2c614259bd
slavroman <slaventiy2000@gmail.com>
2023-03-29 12:36:35 +0900

    Task 1 approved! "Standard C" upgraded to version 17 (2018), task completed but not yet approved

--- HW_30_4/HW_30_4_2/HW_30_4_Task_2.cpp
+++ HW_30_4/HW_30_4_2/HW_30_4_Task_2.cpp
@@ -6,5 +6,22 @@ int main()
 {
 	std::cout << "Task 2.\n";
 
+	cpr::Response r = cpr::Get(cpr::Url{ "http://www.httpbin.org/html" },
+							   cpr::Header{ {"accept", "text/html"} });	
+	
+	std::string openHeader{ "<h1>" }, closeHeader{ "</h1>" };	
+
+	auto posBegin = r.text.find(openHeader);
+	auto posEnd = r.text.find(closeHeader);	 
+	
+	if (posBegin != r.text.npos && posEnd != r.text.npos)
+	{
+		auto it = posBegin + openHeader.size();
+
+		std::string res = r.text.substr(it, posEnd - it);
+
+		std::cout << std::endl << res << std::endl;
+	}	
+
 	return 0;
 }
